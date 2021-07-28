a6940d00c6f2baca82fbfbaaf79d0638437433fa
alessandro <alessandrodigima@gmail.com>
2021-07-27 22:37:17 -0300

    initial

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const MercadoPago = require('mercadopago');
+
+
+MercadoPago.configure({
+    sandbox: true,
+    access_token: "TEST-3260321011293596-061906-6cdf2c22206e79007f91b8f5642b3cf2-267336909"
+})
+
+
+app.get('/', (req, res)=>{
+    res.send('Welcome'+Date.now());
+})
+
+
+app.get('/pagar',async (req,res) =>{
+    //Pagamentos
+    //id // codigo // pagador // status //
+// 1 // 123413411234 // emaildopagador@gmail.com // não foi pago
+// 2 // 123413411234 // emaildopagador@gmail.com // pago
+
+var id = "" + Date.now();
+var emailDoPagador = "victordevtb@gmail.com";
+
+    const dados = {
+        items: [
+            item = {
+                id: id, //UUID ou Date.now()
+                title: '2x Video Games;3x Camisas',
+                quantity: 1,
+                currency_id: 'BRL',
+                unit_price: parseFloat(550)
+            }
+        ],
+        player: {
+            email: emailDoPagador
+        },
+        external_reference: id
+    }
+
+try {
+    var pagamento = await MercadoPago.preferences.create(dados);
+    console.log(pagamento);
+    //Banco.SalvarPagamento({id:id, pagador: emailDoPagador});
+    return res.redirect(pagamento.body.init_point);
+    
+} catch (error) {
+    console.log(error.message);
+}
+    
+})
+
+
+app.listen(3000, ()=>{
+    console.log("listening on port 3000");
+})
